273687e9bc0514770c5b687c85961ea7f5617628
刘鹏 <2519575467@qq.com>
2024-11-04 15:12:27 +0800

    上传es搜索并实现

--- django_agriculture_interlocution/django_agriculture_interlocution/urls.py
+++ django_agriculture_interlocution/django_agriculture_interlocution/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('usermodule.urls')), # 子应用-用户模块
+    path('essearch/', include('essearch.urls')), # 子应用-问题模块
 ]
