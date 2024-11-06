f55db2f14d6309238fd0f1bad48ce1852d564a80
宋金玺 <14719687+sonjinxi@user.noreply.gitee.com>
2024-11-06 09:55:50 +0800

    路由

--- vite_agriculture_interlocution/src/router/index.js
+++ vite_agriculture_interlocution/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import SearchData from '../components/SearchData.vue';
+import HmeView from '../view/HmeView.vue';
+import Service from '../view/Service.vue';
+import ExpertGen from '../view/ExpertGen.vue';
 
 const routes = [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/SearchData', component: SearchData },
+    { path: '/', name: 'Home', component: Home },//主页
+    { path: '/SearchData', component: SearchData },//搜索
+    {path:'/hme',component:HmeView},//服务
+    {path:'/service',component:Service},//选择服务
+    {path:'/expert',name:'expertgen',component:ExpertGen},//专家
 ];
 
 const router = createRouter({
