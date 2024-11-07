9ad083ac00b6129c28b43506bfed7e747deb6812
李硕 <lpsserver443@outlook.com>
2024-11-07 10:22:53 +0800

    前端路由部分调整

--- vite_agriculture_interlocution/src/router/index.js
+++ vite_agriculture_interlocution/src/router/index.js
@@ -13,13 +13,13 @@ import { getAuthorizationCode, fetchAndStoreTokens, isAuthenticated } from '../u
 
 // 定义路由
 const routes = [
-    { path: '/', name: 'Home', component: Home },//主页
+    { path: '/home', name: 'Home', component: Home },//主页
     { path: '/SearchData', component: SearchData },//搜索
-    {path:'/hme',component:HmeView},//服务
-    {path:'/service',component:Service},//选择服务
-    {path:'/expert',name:'expertgen',component:ExpertGen},//专家
-    {path:'/harvesting',name:'harvest',component:HarvestingFrom},//收割服务
-    {path:'/irrigation',component:IrrigationFrom}
+    { path:'/hme',component:HmeView },//服务
+    { path:'/service',component:Service },//选择服务
+    { path:'/expert',name:'expertgen',component:ExpertGen },//专家
+    { path:'/harvesting',name:'harvest',component:HarvestingFrom },//收割服务
+    { path:'/irrigation',component:IrrigationFrom }
 ];
 
 // 创建路由实例
